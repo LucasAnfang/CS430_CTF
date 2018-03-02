8c876d872a15c08bbc482e8f0921f6eb933b6606
Luke Anfang <lanfang@usc.edu>
2018-03-01 16:37:29 -0800

    all transfers now over ssl using self signed cert

--- web_server/server.js
+++ web_server/server.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
+// To get a self signed cert
+// openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 1
+var sslOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    passphrase: 'e77e3fb5'
+};
+const server = https.createServer(sslOptions, app);
+// const server = https.createServer(app);
 
 server.listen(port);
